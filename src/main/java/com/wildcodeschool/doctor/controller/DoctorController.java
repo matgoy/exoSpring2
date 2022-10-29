116538312bf1bb20ce2c93481ba174512a4e2665
matgoy <gohiermathieu78@gmail.com>
2022-10-29 12:32:15 +0200

    solution

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,31 @@
 package com.wildcodeschool.doctor.controller;
 
+
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
-public class DoctorController {
+public class DoctorController extends Doctor {
 
-    @GetMapping("/doctor/")
+
+
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor doctor(@PathVariable int number) {
+        if(number == 13){
+            return new Doctor(13, "Jodie Wittaker");
+        }else if(number >= 1 && number <= 12){
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "Error 303");
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Error 404");
+        }
 
-        return null;
+
+
     }
 }
